82cef85011989158f155458e60f7ed06b2dab9c0
Yeehaa <medialoperations@gmail.com>
2014-04-12 17:55:15 +0200

    switch protractor to firefox

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -4,11 +4,11 @@ exports.config = {
   ],
   // The address of a running selenium server.
   seleniumServerJar: '../node_modules/protractor/selenium/selenium-server-standalone-2.40.0.jar',
-  //seleniumAddress: 'http://localhost:4444/wd/hub',
+  // seleniumAddress: 'http://localhost:4444/wd/hub',
 
   framework: 'mocha',
 
-  baseUrl: 'http://localhost:3000',
+  baseUrl: 'http://127.0.0.0:3000',
 
   plugins: [
     'protractor-coffee-preprocessor'
@@ -16,7 +16,7 @@ exports.config = {
 
   // Capabilities to be passed to the webdriver instance.
   capabilities: {
-    'browserName': 'chrome'
+    'browserName': 'firefox'
   },
 
   // Options to be passed to Jasmine-node.
